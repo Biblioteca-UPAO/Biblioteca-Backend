7ef6d7b32cd43d7a1b10ad560ec103bde8dfbdc1
JamesDroide <james200166@gmail.com>
2023-12-04 01:23:16 -0500

    SolicitudDTO documentation

--- src/main/java/com/upao/biblioteca/domain/dto/solicitudDTO/SolicitudDTO.java
+++ src/main/java/com/upao/biblioteca/domain/dto/solicitudDTO/SolicitudDTO.java
@@ -7,15 +7,44 @@ import lombok.Setter;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 
+/**
+ * Clase que representa un objeto de transferencia de datos (DTO) para una solicitud.
+ */
+
 @Getter
 @Setter
 @AllArgsConstructor
 @NoArgsConstructor
 public class SolicitudDTO {
+
+    /**
+     * Identificador único de la solicitud.
+     */
+
     private Long solicitudId;
+
+    /**
+     * Fecha y hora en que se realizó la solicitud.
+     */
+
     private LocalDateTime fechaSolicitada;
-    private LocalDate fechaRecojo; 
+
+    /**
+     * Fecha en que se planea recoger el libro solicitado.
+     */
+
+    private LocalDate fechaRecojo;
+
+    /**
+     * Título del libro solicitado.
+     */
+
     private String tituloLibro;
+
+    /**
+     * Nombre del usuario que realizó la solicitud.
+     */
+
     private String nombreUsuario;
 }
 
